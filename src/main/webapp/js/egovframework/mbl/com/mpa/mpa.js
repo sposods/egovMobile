2c93c1478edf786f958d5a80d6a5505bbd959dce
michael <michael@DESKTOP-2K3K816>
2026-07-01 14:34:41 +0900

    test

---
+++ src/main/webapp/js/egovframework/mbl/com/mpa/mpa.js
@@ -0,0 +1,93 @@
+/*********************************************************
+ * 초기화
+ ******************************************************** */
+/**
+ * 수정일 			수정자 		수정내용
+ * ---------- --------- ------------------- 
+ * 2024.05.02. 우시재 		NSR 보안조치 ( 사진 제목에 XSS 방지 구현 추가 )
+ */
+var totcnt;
+var imgNum;
+var resultList;
+
+// 20240502 NSR 보안조치 (사진 제목에 XSS 방지 구현 추가 )
+function fn_egov_clearxss_minimum(str){
+	return str.replaceAll("&", "&amp;")
+	.replaceAll("<", "&lt;")
+	.replaceAll(">", "&gt;")
+	.replaceAll("\"", "&#34;")
+	.replaceAll("\'", "&#39;")
+	.replaceAll("\\.", "&#46;")
+	.replaceAll("%2E", "&#46;")
+	.replaceAll("%2F", "&#47;");
+};
+
+function fn_egov_initl_mobilephotolist(){
+	$.getJSON(contextPath + "/mbl/com/mpa/selectMobilePhotoList.mdo", function(json) {
+		var list_html = "";
+		var detail_html = "";
+		resultList = json.resultList;
+
+		totcnt = json.resultList.length;
+		
+		for (var i = 0; i < totcnt; i++) {
+			var result = resultList[i];
+
+			list_html += "<a href=\"javascript\:fn_photodetail('" + i + "')\">";
+			// 20240502 NSR 보안조치 (사진 제목에 XSS 방지 구현 추가 )
+			list_html += "<li><img alt=\"" + fn_egov_clearxss_minimum(result.photoSj) + "\" title =\""+ fn_egov_clearxss_minimum(result.photoSj) + "\" src='" + contextPath + "/cmm/fms/getImage.mdo?atchField=" + result.archField + "&fileSn=0/'></li>";
+			list_html += "</a>";
+		}
+		detail_html += "<li>";
+		detail_html += '<img id="imgSrc" src="" title="" alt=""/>';
+		detail_html += "</li>";
+							
+		$('div[id="photoList"] ul[data-role="listview"]').html(list_html);
+		$('div[id="detailview"]').html(detail_html);
+
+		$('div[id="photoDetail"]').bind('swipeleft ', (function() {
+			imgNum++;
+			if (imgNum > totcnt-1){
+			    imgNum = 0;
+			}
+			// id 값으로 이미지를 바꿈
+			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.mdo?atchFileId=' + resultList[imgNum].atchFileId + '&fileSn=0');
+			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
+			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
+        }));
+        
+		$('div[id="photoDetail"]').bind('swiperight', (function() {
+			imgNum--;
+			if (imgNum < 0){
+				imgNum = totcnt-1;
+			} 
+			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.mdo?atchFileId=' + resultList[imgNum].atchFileId + "&fileSn=0");
+			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
+			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
+        }));
+		
+		$('div[id="photoDetail"]').bind('tap', (function() {
+			imgNum++;
+			if (imgNum > totcnt-1){
+			    imgNum = 0;
+			}
+			// id 값으로 이미지를 바꿈
+			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.mdo?atchFileId=' + resultList[imgNum].atchFileId + '&fileSn=0');
+			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
+			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
+        }));
+		
+	});
+}
+
+/* ********************************************************
+ * 상세화면 처리 함수
+ ******************************************************** */
+function fn_photodetail(index) {
+	$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.mdo?atchFileId=' + resultList[index].atchFileId + '&fileSn=0');
+	$('#imgSrc').attr('title', resultList[index].photoSj);
+	$('#imgSrc').attr('alt', resultList[index].photoSj);
+	
+	imgNum = index;
+	$.mobile.changePage("#photoDetail", "slide", false, false);
+}
